0271312af74f6e4af8c21e9a42acb0f7d86be605
SandeepBallu <ballu.sandeep@gmail.com>
2015-06-27 16:39:41 -0500

    Lab 4- DSKy

---
+++ HelloRomo/HelloRomo/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  HelloRomo
+//
+//
+//  AppDelegate.h
+//  GyrosAndAccelerometers
+//
+//  Created by joseph hoffman on 3/24/13.
+//  Copyright (c) 2013 Joe Hoffman. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ HelloRomo/HelloRomo/ViewController.h
@@ -0,0 +1,49 @@
+//
+//  ViewController.h
+//  HelloRomo
+//
+
+#import <UIKit/UIKit.h>
+#import <RMCore/RMCore.h>
+#import <RMCharacter/RMCharacter.h>
+#import <CoreMotion/CoreMotion.h>
+
+//Accelerometer
+double currentMaxAccelX;
+double currentMaxAccelY;
+double currentMaxAccelZ;
+double currentMaxRotX;
+double currentMaxRotY;
+double currentMaxRotZ;
+
+@interface ViewController : UIViewController <RMCoreDelegate>
+{
+    double speed;
+    Boolean start;
+    Boolean stop;
+}
+@property (nonatomic, strong) RMCoreRobotRomo3 *Romo3;
+@property (nonatomic, strong) RMCharacter *Romo;
+
+//Accelerometer
+@property (strong, nonatomic) IBOutlet UILabel *accX;
+@property (strong, nonatomic) IBOutlet UILabel *accY;
+@property (strong, nonatomic) IBOutlet UILabel *accZ;
+
+@property (strong, nonatomic) IBOutlet UILabel *maxAccX;
+@property (strong, nonatomic) IBOutlet UILabel *maxAccY;
+@property (strong, nonatomic) IBOutlet UILabel *maxAccZ;
+
+@property (strong, nonatomic) IBOutlet UILabel *rotX;
+@property (strong, nonatomic) IBOutlet UILabel *rotY;
+@property (strong, nonatomic) IBOutlet UILabel *rotZ;
+
+@property (strong, nonatomic) IBOutlet UILabel *maxRotX;
+@property (strong, nonatomic) IBOutlet UILabel *maxRotY;
+@property (strong, nonatomic) IBOutlet UILabel *maxRotZ;
+- (void)addGestureRecognizers;
+
+- (IBAction)resetMaxValues:(id)sender;
+
+@property (strong, nonatomic) CMMotionManager *motionManager;
+@end
